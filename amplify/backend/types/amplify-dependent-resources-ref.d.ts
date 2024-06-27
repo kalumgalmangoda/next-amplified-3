91a89f19801dc07673d7ec160a21c71fa5161bd6
kalum.galmangoda <kalum.galmangoda@gmail.com>
2024-06-27 19:45:09 +0530

    Todo app initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nextamplified3": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
